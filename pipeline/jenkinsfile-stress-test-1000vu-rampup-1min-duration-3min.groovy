b52bd5742fc514e692a73048c22ce80298fb2e30
Jean Carlos Bezerra <jean.bezerra@yaman.com.br>
2021-05-09 22:16:09 -0300

    New tests models

---
+++ pipeline/jenkinsfile-stress-test-1000vu-rampup-1min-duration-3min.groovy
@@ -0,0 +1,53 @@
+#!groovy
+
+def BOT_REPOSITORY = "https://github.com/jeancbezerra/app-rest-demo.git"
+def BOT_SCRIPT_NAME = "bot/bot-jmeter-4.jmx"
+def BOT_VIRTUAL_USERS = 1000
+def BOT_RUMPUP = 60
+def BOT_DURATION = 180
+def BOT_STARTUPDELAY = 2
+
+pipeline {
+    agent any
+    tools{
+        jdk "ADOPTOPENJDK_jdk8u292-b10"
+	maven "MAVEN_3_8_1"
+    }
+    options {
+        timestamps()
+        buildDiscarder(logRotator(numToKeepStr:'3'))
+    }
+    stages {
+       stage("Application Repository"){
+            steps {
+			sh 'git config --global http.sslVerify false'
+				timeout(time: 2, unit: "MINUTES") {
+					git branch: "master",
+						credentialsId: "gitlab-yaman",
+						url: "${BOT_REPOSITORY}"
+				}
+            }           
+        }		
+
+        stage("Bot Execution"){
+            steps {
+                sh "/opt/coe/JMETER/apache-jmeter-4.0/bin/jmeter.sh --nongui --testfile ${env.JENKINS_HOME}/workspace/${JOB_NAME}/${BOT_SCRIPT_NAME} --jmeterproperty threads=${BOT_VIRTUAL_USERS} --jmeterproperty rampup=${BOT_RUMPUP} --jmeterproperty duration=${BOT_DURATION} --jmeterproperty startup=${BOT_STARTUPDELAY} --jmeterproperty aggregate=load-test.csv"
+            }
+        }
+
+        	    
+    }
+
+
+    post { 
+        success{ 
+            archiveArtifacts artifacts: 'load-test.csv'
+			cleanWs()
+        }    
+		failure{
+            //archiveArtifacts artifacts: 'load-test.csv'
+		    cleanWs()
+		}
+			
+		}
+}
