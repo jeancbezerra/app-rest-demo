bc793f08fc7423d19bc3f9a1e254a79dbc244fe3
Jean Macena <34484104+jeancbezerra@users.noreply.github.com>
2021-05-09 22:22:34 -0300

    A

--- pipeline/jenkinsfile-deploy-qa.groovy
+++ pipeline/jenkinsfile-deploy-qa.groovy
@@ -1,5 +1,7 @@
 #!groovy
 
+def BOT_REPOSITORY = "https://github.com/jeancbezerra/app-rest-demo.git"
+
 pipeline {
   
     agent any
@@ -15,21 +17,58 @@ pipeline {
     }
   
     stages {
-    stage("Application Repository"){
-      steps {
-        sh 'git config --global http.sslVerify false'
-          timeout(time: 2, unit: "MINUTES") {
-            git branch: "master",
-              credentialsId: "gitlab-yaman",
-              url: "${BOT_REPOSITORY}"
+
+        stage("Application Repository"){
+          steps {
+            sh 'git config --global http.sslVerify false'
+            timeout(time: 2, unit: "MINUTES") {
+                git branch: "master",
+                credentialsId: "gitlab-yaman",
+                url: "${BOT_REPOSITORY}"
+            }
+          }           
+        }
+            
+        stage("Build Application"){
+          steps {
+            sh "mvn clean package -DskipTests"
           }
-         }
-      }           
-    }
+        }
+      
+        //stage("Deploy in Parallel Envs"){
+        //      steps{
+        //        parallel(
+        //            a: {
+        //             deploy adapters: [tomcat9(credentialsId: 'tomcat-user', path: '', url: 'http://192.168.0.23:8080/')], contextPath: 'app-rest-demo', war: '**/app-rest-demo.war'
+        //            },          
+        //            b: {
+        //              deploy adapters: [tomcat9(credentialsId: 'tomcat-user', path: '', url: 'http://192.168.0.70:8080/')], contextPath: 'app-rest-demo', war: '**/app-rest-demo.war'
+        //            }
+        //         )
+        //      }
+        //  }
+      
+      stage("Deploy Parallel Stages"){
+	parallel {
+		//Begin parallel stage qa
+		stage("Deploy in Quality-Assurance"){			
+			steps {
+				deploy adapters: [tomcat9(credentialsId: 'tomcat-user', path: '', url: 'http://192.168.0.70:8080/')], contextPath: 'app-rest-demo', war: '**/app-rest-demo.war'
+			}
+			post {
+				always {
+					echo "Deployed"
+				}
+            }
+		}
+		//End parallel stage qa	
 		
-		stage("Build Application"){
-      steps {
-        sh "mvn clean package -DskipTests"
-      }
+			
+	}
+}
+      
+      
+      
+
     }
 }
