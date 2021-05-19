6a452661b5dee59807a3bac25666c9e48332f4a2
Jean Macena <34484104+jeancbezerra@users.noreply.github.com>
2021-05-19 16:53:52 -0300

    A

--- pipeline/jenkinsfile-load-test.groovy
+++ pipeline/jenkinsfile-load-test.groovy
@@ -9,9 +9,9 @@ pipeline {
 		string(name: "BOT_SCRIPT", defaultValue: "bot/bot-jmeter-4.jmx", description: "Nome do Robo jmx")
 		string(name: "BOT_THREADS", defaultValue: "2", description: "Quantidade de threads (VUs) utilizadas no Robo")
 		string(name: "BOT_RAMPUP", defaultValue: "60", description: "Tempo maximo para subir todas as threads (VUs) durante o teste")
-		string(name: "BOT_DURATION", defaultValue: "120", description: "Tempo de duracao do teste")
+		string(name: "BOT_DURATION", defaultValue: "120", description: "Tempo de duracao do teste em segundos")
 		string(name: "BOT_STARTUPDELAY", defaultValue: "2", description: "Tempo de espera para inicializar o teste")        
-		string(name: "JOB_TIMEOUT", defaultValue: "300", description: "Timeout do Job em segundos")		
+		string(name: "STAGE_TIMEOUT", defaultValue: "300", description: "Timeout do Job em segundos")		
 	}
 	
     tools{
@@ -21,7 +21,6 @@ pipeline {
     options {
         timestamps()
         buildDiscarder(logRotator(numToKeepStr:'3'))
-	    timeout(time: '${JOB_TIMEOUT}', unit: 'SECONDS')
     }
     stages {
        stage("Application Repository"){
@@ -37,7 +36,9 @@ pipeline {
 
         stage("Bot Execution"){
             steps {
-                sh "/opt/coe/JMETER/apache-jmeter-4.0/bin/jmeter.sh --nongui --testfile ${env.JENKINS_HOME}/workspace/${JOB_NAME}/${BOT_SCRIPT} --jmeterproperty threads=${BOT_THREADS} --jmeterproperty rampup=${BOT_RAMPUP} --jmeterproperty duration=${BOT_DURATION} --jmeterproperty startup=${BOT_STARTUPDELAY} --jmeterproperty aggregate=load-test.csv"
+		timeout(time: "${STAGE_TIMEOUT}", unit: "SECONDS"){
+                	sh "/opt/coe/JMETER/apache-jmeter-4.0/bin/jmeter.sh --nongui --testfile ${env.JENKINS_HOME}/workspace/${JOB_NAME}/${BOT_SCRIPT} --jmeterproperty threads=${BOT_THREADS} --jmeterproperty rampup=${BOT_RAMPUP} --jmeterproperty duration=${BOT_DURATION} --jmeterproperty startup=${BOT_STARTUPDELAY} --jmeterproperty aggregate=load-test.csv"
+		}
             }
         }
 
