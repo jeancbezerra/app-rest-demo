930c5dc59650e39be38c27208c4038a85728fa18
Jean Macena <34484104+jeancbezerra@users.noreply.github.com>
2021-05-19 16:33:20 -0300

    A

--- pipeline/jenkinsfile-load-test.groovy
+++ pipeline/jenkinsfile-load-test.groovy
@@ -1,21 +1,18 @@
 #!groovy
 
-
-
-
 pipeline {	
     agent any
 	
-parameters {
-        string(name: "BRANCH", defaultValue: "master", description: "Branch do Repositorio")
-        string(name: "REPOSITORY", defaultValue: "https://github.com/jeancbezerra/app-rest-demo.git", description: "Repositorio do Robo")
-        string(name: "BOT_SCRIPT", defaultValue: "bot/bot-jmeter-4.jmx", description: "Nome do Robo jmx")
-        string(name: "BOT_NUMBER_THREADS", defaultValue: "2", description: "Quantidade de threads (VUs) utilizadas no Robo")
-        string(name: "BOT_LOAD_RAMPUP", defaultValue: "60", description: "Tempo maximo para subir todas as threads (VUs) durante o teste")
-        string(name: "BOT_LOAD_DURATION", defaultValue: "120", description: "Tempo de duracao do teste")
-        string(name: "BOT_EXECUTION_TIMEOUT", defaultValue: "300", description: "Timeout do teste em minutos")
-        string(name: "BOT_STARTUPDELAY", defaultValue: "2", description: "Tempo de espera para inicializar o teste")        
-}
+	parameters {
+		string(name: "BRANCH", defaultValue: "master", description: "Branch do Repositorio")
+		string(name: "REPOSITORY", defaultValue: "https://github.com/jeancbezerra/app-rest-demo.git", description: "Repositorio do Robo")
+		string(name: "BOT_SCRIPT", defaultValue: "bot/bot-jmeter-4.jmx", description: "Nome do Robo jmx")
+		string(name: "BOT_NUMBER_THREADS", defaultValue: "2", description: "Quantidade de threads (VUs) utilizadas no Robo")
+		string(name: "BOT_LOAD_RAMPUP", defaultValue: "60", description: "Tempo maximo para subir todas as threads (VUs) durante o teste")
+		string(name: "BOT_LOAD_DURATION", defaultValue: "120", description: "Tempo de duracao do teste")
+		string(name: "BOT_EXECUTION_TIMEOUT", defaultValue: "300", description: "Timeout do teste em minutos")
+		string(name: "BOT_STARTUPDELAY", defaultValue: "2", description: "Tempo de espera para inicializar o teste")        
+	}
 	
     tools{
         jdk "ADOPTOPENJDK_jdk8u292-b10"
@@ -40,7 +37,7 @@ parameters {
 
         stage("Bot Execution"){
             steps {
-                sh "/opt/coe/JMETER/apache-jmeter-4.0/bin/jmeter.sh --nongui --testfile ${env.JENKINS_HOME}/workspace/${JOB_NAME}/${BOT_SCRIPT_NAME} --jmeterproperty threads=${BOT_VIRTUAL_USERS} --jmeterproperty rampup=${BOT_RUMPUP} --jmeterproperty duration=${BOT_DURATION} --jmeterproperty startup=${BOT_STARTUPDELAY} --jmeterproperty aggregate=load-test.csv"
+                sh "/opt/coe/JMETER/apache-jmeter-4.0/bin/jmeter.sh --nongui --testfile ${env.JENKINS_HOME}/workspace/${JOB_NAME}/${BOT_SCRIPT} --jmeterproperty threads=${BOT_VIRTUAL_USERS} --jmeterproperty rampup=${BOT_RUMPUP} --jmeterproperty duration=${BOT_DURATION} --jmeterproperty startup=${BOT_STARTUPDELAY} --jmeterproperty aggregate=load-test.csv"
             }
         }
 
